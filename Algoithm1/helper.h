0f0c2ee7cd59354d0fc3e85e42ec8eafefe3673c
Afshin Loni <afshin.loni@gmail.com>
2014-08-31 19:25:19 +0200

    Helper Updated

---
+++ Algoithm1/helper.h
@@ -0,0 +1,100 @@
+#ifndef _HELPER_H_
+#define _HELPER_H_
+
+/* includefiles */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+#include <string.h>
+#include <float.h>
+#include <time.h>
+
+
+
+int min( int a, int b);
+int max( int a, int b);
+double fmin( double a, double b);
+double fmax( double a, double b);
+
+
+
+/**
+ * Error handling:
+ *
+ * ERROR(s) writes an error message and terminates the program
+ *
+ * Example:
+ * ERROR("File not found !");
+ */
+#define ERROR(s)    errhandler( __LINE__, __FILE__, s)
+
+/**
+ * Error handling:
+ *
+ * ERROR(s) writes an error message and terminates the program
+ *
+ * Example:
+ * ERROR("File not found !");
+ */
+#define ERROUT stdout
+
+/**
+ * Error handling:
+ *
+ * ERROR(s) writes an error message and terminates the program
+ *
+ * Example:
+ * ERROR("File not found !");
+ */
+void  errhandler( int nLine, const char *szFile, const char *szString );
+
+
+
+
+/**
+ * Reading from a datafile.
+ *
+ * The foloowing three macros help reading values from the parameter file.
+ * If a variable cannot be found, the program stops with an error message.
+ *
+ * Example:
+ * READ_INT( "MyFile.dat", imax );
+ * READ_STRING( szFile, szProblem );
+ */
+#define READ_INT( szFileName, VarName)    read_int   ( szFileName, #VarName, &(VarName) )
+
+/**
+ * Reading from a datafile.
+ *
+ * The foloowing three macros help reading values from the parameter file.
+ * If a variable cannot be found, the program stops with an error message.
+ *
+ * Example:
+ * READ_INT( "MyFile.dat", imax );
+ * READ_STRING( szFile, szProblem );
+ */
+#define READ_DOUBLE( szFileName, VarName) read_double( szFileName, #VarName, &(VarName) )
+
+/**
+ * Reading from a datafile.
+ *
+ * The foloowing three macros help reading values from the parameter file.
+ * If a variable cannot be found, the program stops with an error message.
+ *
+ * Example:
+ * READ_INT( "MyFile.dat", imax );
+ * READ_STRING( szFile, szProblem );
+ */
+#define READ_STRING( szFileName, VarName) read_string( szFileName, #VarName,  (VarName) )
+
+void read_string( const char* szFilename, const char* szName, char*  sValue);
+void read_int   ( const char* szFilename, const char* szName, int*    nValue);
+void read_double( const char* szFilename, const char* szName, double*  Value);
+
+
+
+
+
+
+#endif
